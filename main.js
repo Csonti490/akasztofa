307b6ea2552e46dcc50134308f709710bfa533be
CsontiXD <113976041+Csonti490@users.noreply.github.com>
2024-12-21 17:35:32 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 
 function CimCsere(){
 
-    var c = document.getElementById("cim");
+    let c = document.getElementById("cim");
     c.innerHTML = "__________"; c.innerHTML = "Akasztófa";
     const a = "Akasztófa";
-    const abc = ['A', 'Á', 'B', 'C', 'D', 'E', 'É', 'F', 'G', 'H', 'I', 'Í', 'J', 'K', 'L', 'M', 'N', 'O', 'Ó', 'Ö', 'Ő', 'P', 'R', 'S', 'T', 'U', 'Ú', 'Ü', 'Ű', 'V', 'Z'];
+    const abc = ['A', 'Á', 'B', 'C', 'D', 'E', 'É', 'F', 'G', 'H', 'I', 'Í', 'J', 'K', 'L', 'M', 'N', 'O', 'Ó', 'Ö', 'Ő', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ú', 'Ü', 'Ű', 'V', 'W', 'X', 'Y', 'Z'];
     
 }
+var betumeret = 10;
 
 var alapok = document.getElementById("alapok");
 var jatekter = document.getElementById("jatekter");
@@ -24,7 +25,7 @@ window.onload = function() {
       input.value = '';
     });
     document.querySelectorAll('input[name="nehezseg"]')[0].checked = true;
-    document.getElementById("b_meret").value = 96;
+    document.getElementById("b_meret").value = betumeret;
     Gyari();
 };
 
@@ -37,9 +38,11 @@ function Mutat(){
     if(szem.innerHTML == sz[0]){
         document.getElementById("szo").type = "password";
         szem.innerHTML = sz[1];
+        document.getElementById("szo").placeholder = "Kitalálandó szó (rejtve)";
     }else{
-        szem.innerHTML = sz[0];
         document.getElementById("szo").type = "text";
+        szem.innerHTML = sz[0];
+        document.getElementById("szo").placeholder = "Kitalálandó szó (mutatva)";
     }
     /*szem.innerHTML = szem.innerHTML==sz[0]?sz[1]:sz[0];
     document.getElementById("szo").type = szem.innerHTML==sz[0]?"text":"password";*/
@@ -87,9 +90,28 @@ function Kezdes(){
     }
 }
 
+var y = '<i class="fa-solid fa-check text-success"></i>';
+var n = '<i class="fa-solid fa-xmark text-danger"></i>';
+
+document.getElementById('szo').addEventListener('input', function (e) {
+    // Cseréljük le az összes "_" karaktert üres stringre
+    e.target.value = e.target.value.replace(/_/g, '');
+    if (e.target.value.length == 0){
+        document.getElementById("hibaellenorzes1").innerHTML = n + " Nincs beírva semmi.";
+        StartEngedelyezes(true);
+    }else{
+        document.getElementById("hibaellenorzes1").innerHTML = y + " Gomb engedélyezve.";
+        StartEngedelyezes(false);
+    }
+});
+
+function StartEngedelyezes(yn){
+    document.getElementById("start").disabled = yn;
+}
+
 /* Csak a megfelelő karakterek elfogadása */
 document.getElementById('betuinput').addEventListener('input', function (e) {
-    var pattern = /^[A-Za-z0-9ÁÉÍÓÖŐÚÜŰáéíóöőúüű]+$/;
+    let pattern = /^[A-Za-z0-9ÁÉÍÓÖŐÚÜŰáéíóöőúüű]+$/;
     
     if (!pattern.test(e.target.value)) {
         e.target.value = e.target.value.replace(/[^A-Za-z0-9ÁÉÍÓÖŐÚÜŰáéíóöőúüű]/g, '');
@@ -148,8 +170,8 @@ function Ellenoriz(){
 
 /* Akasztófa kinézete */
 function EmberValt(){
-    var maxelet = parseInt(document.querySelector('input[name="nehezseg"]:checked').value);
-    var emberkiir = document.getElementById("emberkep");
+    let maxelet = parseInt(document.querySelector('input[name="nehezseg"]:checked').value);
+    let emberkiir = document.getElementById("emberkep");
     
     switch (maxelet) {
         case 16:
@@ -262,8 +284,8 @@ var cim = document.getElementById("megjelenito");
 var b_meret = document.getElementById("b_meret");
 b_meret.addEventListener("input", function() {
     var meret = b_meret.value;
-    cim.style.fontSize = meret + "px";
-    document.getElementById("ertek").innerText = meret+" px";
+    cim.style.fontSize = meret + "vw";
+    document.getElementById("ertek").innerText = meret+" vw";
 });
 
 /* Ideiglenes téma */
@@ -313,9 +335,9 @@ function UjJatek(){
 
 /* Beállítások alaphelyzete */
 function Gyari(){
-    document.getElementById("megjelenito").style.fontSize = 96 + "px";
-    document.getElementById("b_meret").value = 96;
-    document.getElementById("ertek").innerHTML = 96 + " px";
+    document.getElementById("megjelenito").style.fontSize = betumeret + "vw";
+    document.getElementById("b_meret").value = betumeret;
+    document.getElementById("ertek").innerHTML = betumeret + " vw";
     let gomb = document.getElementsByClassName("temagomb");
     gomb[0].disabled = true;
     gomb[1].disabled = false;
